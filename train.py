6761f76e551fb63c084d59bcf40ac9242ef45d07
void4 <arndtschnabel@freenet.de>
2020-09-02 10:06:40 +0200

    Initial commit

---
+++ train.py
@@ -0,0 +1,37 @@
+from flask import send_from_directory
+from glob import glob
+from flask import Flask
+import os
+from random import choice
+
+app = Flask(__name__)
+
+def imagesWith(prefix):
+    types = "png jpg jpeg".split()
+    images = []
+    path = "static/img"
+    for type in types:
+        images.extend(glob(os.path.join(path, prefix+"*."+type)))
+    return images
+
+@app.route('/')
+def index():
+    return send_from_directory("static", "train.html")
+
+@app.route("/img/<path>")
+def img_path(path):
+    return send_from_directory("static/img/", path)
+
+@app.route('/ground')
+def img_ground():
+    return send_from_directory("static", choice(imagesWith("ground_")))
+
+@app.route('/air')
+def img_air():
+    path = choice(imagesWith("air_"))
+    return send_from_directory(*os.path.split(path))
+
+
+
+if __name__ == "__main__":
+    app.run()
